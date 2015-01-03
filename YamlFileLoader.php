f2639ca5619f5f62892fc2993cb8d5d490f152cb
Craig Heydenburg <craigh@mac.com>
2015-01-03 14:32:20 -0500

    update docblocks again

--- YamlFileLoader.php
+++ YamlFileLoader.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * Copyright Zikula Foundation 2014 - Zikula CoreInstaller bundle.
+ * Copyright Zikula Foundation 2014.
  *
  * This work is contributed to the Zikula Foundation under one or more
  * Contributor Agreements and licensed to You under the following license:
  *
  * @license MIT.
  * @package Zikula
+ * @author Craig Heydenburg
  *
- * Please see the NOTICE file distributed with this source code for further
+ * Please see the LICENSE file distributed with this source code for further
  * information regarding copyright and licensing.
  */
 
